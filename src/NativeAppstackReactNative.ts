c33dca72af1f8e593b1fd6e9eb2b265ba91419b1
Tom Redon Levesque <tom.redon@app-stack.tech>
2025-09-18 10:19:23 +0200

    Fix: patch the global signature for the SDK

--- src/NativeAppstackReactNative.ts
+++ src/NativeAppstackReactNative.ts
@@ -2,7 +2,7 @@ import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
-  configure(apiKey: string): Promise<boolean>;
+  configure(apiKey: string, isDebug?: boolean, endpointBaseUrl?: string, logLevel?: number): Promise<boolean>;
   sendEvent(eventName: string, eventType: string, revenue?: number): Promise<boolean>;
   enableAppleAdsAttribution(): Promise<boolean>;
 }
