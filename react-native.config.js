928a787b87290a635d62670bc769fcc0e203c828
Tom Redon Levesque <tom.redon@app-stack.tech>
2025-10-02 16:27:30 +0200

    Fix: upgrade some parts to make it compatible with expo 54

--- react-native.config.js
+++ react-native.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-  dependencies: {
-    'react-native-appstack-sdk': {
-      platforms: {
-        android: {
-          sourceDir: '../android',
-          packageImportPath: 'import com.appstack.reactnative.AppstackReactNativePackage;',
-          packageInstance: 'new com.appstack.reactnative.AppstackReactNativePackage()',
-        },
-        ios: {
-          // iOS configuration is handled by the podspec
-        },
+  dependency: {
+    platforms: {
+      android: {
+        sourceDir: './android',
+        packageImportPath: 'import com.appstack.reactnative.AppstackReactNativePackage;',
+        packageInstance: 'new com.appstack.reactnative.AppstackReactNativePackage()',
+        // Disable CMake autolinking - native build is handled by build.gradle externalNativeBuild
+        // This prevents autolinking from trying to include codegen before it's generated
+        cmakeListsPath: null,
+      },
+      ios: {
+        // iOS configuration is handled by the podspec
       },
     },
   },
