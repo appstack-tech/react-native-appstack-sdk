e61edabcb4447f2d8cd7a5b004c0a896a27967f7
Tom Redon Levesque <tom.redon@app-stack.tech>
2025-10-02 11:33:52 +0200

    Fix: make it compatible with Expo SDK 54

--- src/NativeAppstackReactNative.ts
+++ src/NativeAppstackReactNative.ts
@@ -1,5 +1,5 @@
 import type { TurboModule } from 'react-native';
-import { TurboModuleRegistry } from 'react-native';
+import { TurboModuleRegistry, NativeModules } from 'react-native';
 
 export interface Spec extends TurboModule {
   configure(apiKey: string, isDebug?: boolean, endpointBaseUrl?: string, logLevel?: number): Promise<boolean>;
@@ -7,4 +7,7 @@ export interface Spec extends TurboModule {
   enableAppleAdsAttribution(): Promise<boolean>;
 }
 
-export default TurboModuleRegistry.getEnforcing<Spec>('AppstackReactNative');
+// Support both old and new architecture
+const AppstackReactNativeModule = TurboModuleRegistry.getEnforcing<Spec>('AppstackReactNative');
+
+export default AppstackReactNativeModule || NativeModules.AppstackReactNative;
